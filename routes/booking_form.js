63f1d61730931652de64779764d48685494b2478
shreyansh <shreyansh.barodiya@gmail.com>
2016-11-18 18:34:23 +0530

    Booking form front-end

--- routes/booking_form.js
+++ routes/booking_form.js
@@ -5,9 +5,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.render('booking_form', {title: "Booking form"});
-});
+router.get('/:train_no/:coach_class/:from/:to/:journeydate/:boardingdate', function (req, res) {
+    var train_no = req.params.train_no;
+    var coach_class = req.params.coach_class;
+    var from = req.params.from;
+    var to = req.params.to;
+    var journey_date = req.params.journeydate;
+    var boarding_date = req.params.boardingdate;
 
+    res.render('booking_form',
+        {
+            title: "Book Ticket",
+            train_no: train_no,
+            coach_class: coach_class,
+            from : from,
+            to : to,
+            journey_date : journey_date,
+            boarding_date : boarding_date
+        });
+});
 module.exports = router;
